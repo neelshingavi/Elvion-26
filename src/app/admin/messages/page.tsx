af3a39a6792b97a06cc2e986afdc413ab7bf6582
Neel Shingavi <shingavineel@gmail.com>
2026-01-24 23:50:57 +0530

    UI updates

--- src/app/admin/messages/page.tsx
+++ src/app/admin/messages/page.tsx
@@ -3,9 +3,5 @@
 import { MessagingHub } from "@/components/messaging/MessagingHub";
 
 export default function AdminMessagesPage() {
-    return (
-        <div className="fixed inset-0 md:left-64 p-4 bg-zinc-950 z-10 overflow-hidden flex flex-col">
-            <MessagingHub roleContext="admin" />
-        </div>
-    );
+    return <MessagingHub roleContext="admin" />;
 }
